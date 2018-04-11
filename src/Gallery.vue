0030ec6335445f04777f97e46d096202440014fe
Primo <peeprimo@gmail.com>
2018-04-12 00:31:10 +0700

    impove input field

--- src/Gallery.vue
+++ src/Gallery.vue
@@ -47,16 +47,16 @@
               <div class="card-content">
                 <div class="media">
                   <div class="">
-                    <input class="title is-4 input" placeholder="TOPIC">
+                    <input v-model="post.inputTopic" class="title is-4 input" placeholder="TOPIC">
                     <div @click="triggerDropdown()" :class="activeClass" class="dropdown" style="width:100%">
                       <div class="dropdown-trigger" style="width:100%">
                         <button class="button" aria-haspopup="true" aria-controls="dropdown-menu" style="width:100%">
-                          <span>{{selectedMember}}</span>
+                          <span>{{post.selectedMember}}</span>
                         </button>
                       </div>
                       <div class="dropdown-menu" id="dropdown-menu" role="menu">
                         <div class="dropdown-content">
-                          <a v-for="(i, index) in dropdownArray" :key=index @click="selectedMember=i" :class="activeItem" class="dropdown-item">
+                          <a v-for="(i, index) in dropdownArray" :key=index @click="post.selectedMember=i" :class="activeItem" class="dropdown-item">
                             {{i}}
                           </a>
                         </div>
@@ -65,7 +65,7 @@
                   </div>
                 </div>
                 <div class="content">
-                  <textarea rows="5" class="textarea"></textarea>
+                  <textarea v-model="post.inputContent" rows="5" class="textarea"></textarea>
                   <br>
                   <time datetime="2016-1-1">{{ dateandtime }}</time>
                   <br><br>
@@ -75,27 +75,26 @@
             </div>
           </div>
         </transition>
-        <div v-for="(obj,index) in postArray" :key="index" class="post shadow" style="z-index:-1">
+        <div v-for="(post,index) in postArray" :key="index" class="post shadow">
           <div class="card ">
             <div class="card-image">
               <figure class="image inbox">
-                <img :src="obj.imgURL" alt="Placeholder image">
+                <img :src="post.imgURL" alt="Placeholder image">
               </figure>
             </div>
             <div class="card-content">
               <div class="media">
                 <div class="">
-                  <p class="title is-4">BNK48</p>
-                  <p class="subtitle is-6">index@{{index}}</p>
+                  <p class="title is-4">{{post.inputTopic}}</p>
+                  <p class="subtitle is-6">{{post.selectedMember}}@{{index}}</p>
                 </div>
               </div>
               <div class="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                <a>@bulmaio</a>.
-                <a href="#">#css</a>
-                <a href="#">#responsive</a>
-                <br>
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                <p>{{post.inputContent}}</p>
+                <a href="#">#{{post.inputTopic}}</a>
+                <a href="#">#{{post.selectedMember}}</a>
+                <br><br>
+                <time datetime="2016-1-1">{{post.datetime}}</time>
               </div>
             </div>
           </div>
@@ -117,16 +116,16 @@
         resImgURL: 'https://bulma.io/images/placeholders/128x128.png',
         selectedFile: null,
         post: {
-          topic: 'dummy topic',
-          content: 'dummy content',
-          name: 'BNK48',
-          imgURL: ''
+          inputTopic:"BNK48",
+          inputContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+          selectedMember: "Select Member",
+          imgURL: '',
+          datetime: null
         },
         postArray: [],
-        dropdownArray: ["Can", "Cherprang", "Izurina", "Jaa", "Jane", "Jennis", "Jib", "Kaew", "Kaimook"],
+        dropdownArray: ["Can", "Cherprang", "Izurina", "Jaa", "Jane", "Jennis", "Jib", "Kaew", "Kaimook", "Orn"],
         activeClass: "",
         activeItem: "",
-        selectedMember: "Select Member",
         dateandtime: new Date().toLocaleString(),
         filePath: './assets/',
         fileName: 'Add-Icon-5.png'
@@ -147,6 +146,7 @@
             this.resData = res.data.message
             this.resImgURL = res.data.downloadURL
             this.post.imgURL = res.data.downloadURL
+            this.post.datetime = new Date().toLocaleString()
             return res.data.downloadURL
           }).then(URL => {
             axios.post('https://ratchaponproject.firebaseio.com/data.json', this.post)
@@ -226,6 +226,10 @@
     /* Actually FF 20+ */
   }
 
+  .content {
+    word-wrap: break-word;
+  }
+
   .shadow {
     border-color: thistle;
     filter: drop-shadow(0px 0px .3rem thistle);
@@ -234,10 +238,6 @@
   .shadow:hover {
     border-color: #00d1b2;
     filter: drop-shadow(0px 0px .3rem #00d1b2);
-  }
-
-  .post {
-    overflow: hidden;
   }
 
   .card {
@@ -327,10 +327,9 @@
 @keyframes bounce-in {
   0% {
     transform: scale(0);
-    /* transform: translateY(-100%); */
+    
   }
   100% {
-    /* transform: translateY(0); */
     transform: scale(1);
   }
 }
